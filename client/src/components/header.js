4acab07c5700928a61616c593890d8dd9cdaa14a
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-12-07 15:44:13 -0600

    navbar almost done

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,21 +1,36 @@
 import '../general.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import HomeIcon from '@mui/icons-material/HomeRounded';
+import LogoutIcon from '@mui/icons-material/LogoutRounded';
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
 
-  return(
-    <>
+  //grab auth state
+  const authenticated = useSelector(state => state.auth.authenticated);
+
+  //change navbar in user is logged in
+  if (authenticated) {
+    return (
       <div>
         <ul id="nav">
-          <li><a href="/">Home</a></li>
-          <li><a href="/login">Log In</a></li>
-          <li><a href="/signup">Sign Up</a></li>
+          <li><a href="/"><HomeIcon /></a></li>
+          <li id='auth-nav'><a href="/"><LogoutIcon /></a></li>
         </ul>
       </div>
-    </>
+    )
+  }
+
+  return(
+    <div>
+      <ul id="nav">
+        <li><a href="/"><HomeIcon /></a></li>
+        <li><a href="/login">Log In</a></li>
+        <li>
+          <a href='/signup'>Sign Up</a>
+        </li>
+      </ul>
+    </div>
   )
 };
 
