e2bbf9628925bd22e614df912c17fad0a06294ba
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-12-21 11:01:14 -0600

    Graph working - norm data

--- client/src/components/progress.js
+++ client/src/components/progress.js
@@ -1,8 +1,30 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { currentUser } from "../actions";
 import Header from "./headers/header-reg";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
 //store all data in correct formats
 var dataOverlord = {
@@ -18,13 +40,15 @@ const Progress = () => {
     dispatch(currentUser());
   }, []);
 
-  const progress = useSelector(state => state.user.perfectPitch);
-  while(progress === undefined) {
+  let Progress = useSelector(state => state.user);
+  while(Progress.perfectPitch === undefined) {//so whole thing doesn't freak out
     return null;
   };
 
+  let progress = Progress.perfectPitch; 
+
   //formats dataOverlord 
-  const answers = () => {
+  const dataFormat = () => {
     let answerHolder = [];
     let sessionHolder = 0;
     progress.map(e => {
@@ -41,24 +65,82 @@ const Progress = () => {
     dataOverlord.answers = answerHolder;
     dataOverlord.session = sessionHolder;
   }
-  answers();
-  
-
-  //grabs answers by note
-  const answersByNote = () => {
-    progress.map(e => {
-      e.answers.map(p => {
-
-      })
-    })
-  }
+  dataFormat();
   console.log(dataOverlord);
+
+  // const data = [
+  //   { year: 2010, count: 10 },
+  //   { year: 2011, count: 20 },
+  //   { year: 2012, count: 15 },
+  //   { year: 2013, count: 25 },
+  //   { year: 2014, count: 22 },
+  //   { year: 2015, count: 30 },
+  //   { year: 2016, count: 28 },
+  // ];
+
+  // //chart info
+  // new Chart(
+  //   document.getElementById('chart'),
+  //   {
+  //     type: 'bar',
+  //     data: {
+  //       labels: data.map(row => row.year),
+  //       datasets: [
+  //         {
+  //           label: 'Acquisitions by year',
+  //           data: data.map(row => row.count)
+  //         }
+  //       ]
+  //     }
+  //   }
+  // );
+
+
+
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'User Data',
+      },
+    },
+  };
+  
+  const labels = dataOverlord.answers.map(e => e.note)
+  let baseline = 100;
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Total',
+        data: dataOverlord.answers.map(e => {
+          // debugger
+          baseline = baseline + e.correct
+          return baseline;
+        }),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+
+
+
+
   return (
     <div>
       <Header />
-      <div className="mx-auto" style={{width: '500px'}}>
+      <div className="mx-auto" style={{width: '1000px'}}>
         advertising doesn't work? JUST DID LMAO fat fat fat fat fat fat fat fat fat fat fat 
-        
+      </div>
+      <div className="mx-auto" style={{width: '1000px'}}>
+        <Line options={options} data={data} />
       </div>
     </div>
   );
